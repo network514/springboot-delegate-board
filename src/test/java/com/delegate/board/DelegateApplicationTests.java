e1c57963fe038e83b7bc718ee9ca2ee61c3389b2
권자영 <jykwon@minigate.net>
2019-02-12 13:38:28 +0900

    Project init

---
+++ src/test/java/com/delegate/board/DelegateApplicationTests.java
@@ -0,0 +1,17 @@
+package com.delegate.board;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DelegateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
